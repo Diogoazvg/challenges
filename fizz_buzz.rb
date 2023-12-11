21d46b7164ab594ad6a2cb3846b7956ff56f3c31
Majin Vegeta <diogo.azvg@gmail.com>
2023-12-11 16:59:08 -0300

    Problem:Write a program that prints the numbers from 1 to 100. But for multiples of three, print 'Fizz' instead of the number, and for the multiples of five, print 'Buzz'. For numbers that are multiples of both three and five, print 'FizzBuzz'.

---
+++ fizz_buzz.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+# frozen_string_literal: true
+
+puts 'Enter a final range should be a integer'
+range = gets.chomp
+
+def fizz_buzz(r)
+  (1..r).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts 'FizzBuzz'
+    elsif num % 3 == 0
+      puts 'Fizz'
+    elsif num % 5 == 0
+      puts 'Buzz'
+    else
+      puts num
+    end
+  end
+end
+
+fizz_buzz(range.to_i)
