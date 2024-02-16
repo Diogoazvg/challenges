6bbf5bf2cc88a5d950544801fb159026732c2e11
Majin Vegeta <diogo.azvg@gmail.com>
2024-02-16 14:49:09 -0300

    Optimize FizzBuzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,20 +1,37 @@
 # frozen_string_literal: true
 
-puts 'Enter a final range should be a integer'
+puts 'Enter a number, should be a integer'
 range = gets.chomp
 
-def fizz_buzz(r)
-  (1..r).each do |num|
-    if num % 3 == 0 && num % 5 == 0
-      puts 'FizzBuzz'
-    elsif num % 3 == 0
-      puts 'Fizz'
-    elsif num % 5 == 0
-      puts 'Buzz'
+def fizz_buzz(number)
+  (1..number).map do |num|
+    fizz = (num % 3).zero?
+    buzz = (num % 5).zero?
+    if fizz && buzz
+      'Fizz'
+    elsif fizz
+      'Fizz'
+    elsif buzz
+      'Buzz'
     else
-      puts num
+      num
     end
   end
 end
+p fizz_buzz(range.to_i)
 
-fizz_buzz(range.to_i)
+# Without map approach
+
+# def fizz_buzz(number)
+#   (1..number).each do |num|
+#     if (num % 3).zero? && (num % 5).zero?
+#       puts 'FizzBuzz'
+#     elsif (num % 3).zero?
+#       puts 'Fizz'
+#     elsif (num % 5).zero?
+#       puts 'Buzz'
+
+#       puts num
+#     end
+#   end
+# end
